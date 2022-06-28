3214c9c998bef16314d009a0293a06b74530b9e3
MrKenobii <anldygc35@windowslive.com>
2022-06-28 18:26:09 +0300

    First Controller and Web View on .jsp file

---
+++ src/main/java/com/anilduyguc/HelloController.java
@@ -0,0 +1,14 @@
+package com.anilduyguc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HelloController {
+
+    @RequestMapping( "/")
+    public String getPage(){
+        return "hello_world";
+    }
+}
